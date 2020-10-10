db21e5fc61023f2836d6ddcf80467ce55715c6e8
TetsuFe <tetsubeg1ner27@gmail.com>
2020-10-10 20:18:24 +0900

    Add jwt auth api endpoint

--- app/Http/Controllers/GachaController.php
+++ app/Http/Controllers/GachaController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-
 class GachaController extends Controller
 {
    public function platinum(){
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GachaController;
+use App\Http\Controllers\Auth\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +16,25 @@ use App\Http\Controllers\GachaController;
 |
 */
 
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::get('/gacha/platinum',[GachaController::class, 'platinum']);
+Route::post('/register', [RegisterController::class, 'register']);
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
+
