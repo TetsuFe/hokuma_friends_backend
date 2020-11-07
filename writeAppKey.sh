91fdff90650ff0e038271fc70c02e2e4e76c27c3
TetsuFe <tetsubeg1ner27@gmail.com>
2020-11-07 11:32:10 +0900

    Change key settings method

--- writeAppKey.sh
+++ writeAppKey.sh
@@ -1,16 +1,3 @@
 #!/bin/sh
 
-while IFS="" read -r p || [ -n "$p" ]
-do
-  if printf '%s' "$p" | grep -Eq '^APP_KEY'; then
-    key=$(php artisan key:generate --show)
-    echo "APP_KEY=$key" >> .envv
-    printf '%s\n' "APP_KEY generated and set"
-  else
-    echo "\n$p" >> .envv
-  fi
 
-done < .env
-
-cat .envv >> .env
-rm .envv
