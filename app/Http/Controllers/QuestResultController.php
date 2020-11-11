c8296bbcfe0590ca829743394e58bfece88c0468
TetsuFe <tetsubeg1ner27@gmail.com>
2020-11-12 04:00:29 +0900

    Add check if duplicated record exists

--- app/Http/Controllers/QuestResultController.php
+++ app/Http/Controllers/QuestResultController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\QuestResult;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
 
 class QuestResultController extends Controller
 {
@@ -13,8 +12,11 @@ class QuestResultController extends Controller
     {
         $questId = $request->input('questId');
         $isCleared = $request->input('isCleared');
-        Log::debug([$questId, $isCleared]);
-        $questResult = QuestResult::query()->create(['user_id'=>Auth::user()->id, 'questId'=> $questId, 'isCleared'=>$isCleared]);
-        return response()->json(['user_id'=> $questResult->user()[0]->id, "questId" => $questResult->questId, 'isCleared'=>$questResult->isCleared]);
+        if (QuestResult::query()->where('questId', $questId)->where('user_id', Auth::user()->id)->count() == 0) {
+            $questResult = QuestResult::query()->create(['user_id' => Auth::user()->id, 'questId' => $questId, 'isCleared' => $isCleared]);
+            return response()->json(['user_id' => $questResult->user()[0]->id, "questId" => $questResult->questId, 'isCleared' => $questResult->isCleared]);
+        } else {
+            return response()->json(['user_id' => Auth::user()->id, "questId" => $questId, 'isCleared' => $isCleared]);
+        }
     }
 }
