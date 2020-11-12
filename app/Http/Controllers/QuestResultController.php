0e42a68367d8e2537331c650af38aa850f080d50
TetsuFe <tetsubeg1ner27@gmail.com>
2020-11-12 12:23:52 +0900

    Add update quest result feature

--- app/Http/Controllers/QuestResultController.php
+++ app/Http/Controllers/QuestResultController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\QuestResult;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class QuestResultController extends Controller
 {
@@ -12,11 +13,18 @@ class QuestResultController extends Controller
     {
         $questId = $request->input('questId');
         $isCleared = $request->input('isCleared');
-        if (QuestResult::query()->where('questId', $questId)->where('user_id', Auth::user()->id)->count() == 0) {
+        $oldQuestResults = QuestResult::query()->where('questId', $questId)->where('user_id', Auth::user()->id);
+        if ($oldQuestResults->count() == 0) {
             $questResult = QuestResult::query()->create(['user_id' => Auth::user()->id, 'questId' => $questId, 'isCleared' => $isCleared]);
             return response()->json(['user_id' => $questResult->user()[0]->id, "questId" => $questResult->questId, 'isCleared' => $questResult->isCleared]);
         } else {
-            return response()->json(['user_id' => Auth::user()->id, "questId" => $questId, 'isCleared' => $isCleared]);
+            $oldQuestResult = $oldQuestResults->get()->first();
+            if(!$oldQuestResult->isCleared && $isCleared){
+                $questResult = $oldQuestResults->update(['user_id' => Auth::user()->id, 'questId' => $questId, 'isCleared' => $isCleared]);
+                return response()->json(['user_id' => $questResult->user()[0]->id, "questId" => $questResult->questId, 'isCleared' => $questResult->isCleared]);
+            }else{
+                return response()->json(['user_id' => Auth::user()->id, "questId" => $questId, 'isCleared' => $isCleared]);
+            }
         }
     }
 }
