213ef2b9a0a5db6573593b07caf7a22826b6f3e0
TetsuFe <tetsubeg1ner27@gmail.com>
2020-11-13 13:32:34 +0900

    Fix my characters temporary implementation

--- app/Http/Controllers/GachaController.php
+++ app/Http/Controllers/GachaController.php
@@ -8,12 +8,13 @@ use Illuminate\Support\Facades\Auth;
 class GachaController extends Controller
 {
    public function platinum(){
-       $user = Auth::user();
-       $characters = UserCharacter::query()->where('user_id', $user->id)->get();
-       $charactersRef = $user->characters();
        $randomCharacterId = random_int(1, 5);
        $newCharacter = new UserCharacter(['characterId' => $randomCharacterId]);
-       if($characters->count() == 0){
+
+       $user = Auth::user();
+       $characters = UserCharacter::query()->where('user_id', $user->id)->get();
+       if(!$characters->contains($randomCharacterId)){
+           $charactersRef = $user->characters();
            $charactersRef->save($newCharacter);
        }
        return response()->json(["characterId"=> $newCharacter->characterId]);
