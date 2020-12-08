a298823681f2eadea9434f17c57108480b7008ba
TetsuFe <tetsubeg1ner27@gmail.com>
2020-12-08 09:40:38 +0900

    Retern jwt to use client

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,31 +2,32 @@
 
 namespace App\Http\Controllers\Auth;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Laravel\Socialite\Facades\Socialite;
 use App\User;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends \App\Http\Controllers\Controller
 {
-    public function redirectToTwitterProvider(){
+    public function redirectToTwitterProvider()
+    {
         return Socialite::driver('twitter')->redirect();
     }
 
-    public function handleTwitterProviderCallback(){
+    public function handleTwitterProviderCallback()
+    {
         try {
             $user = Socialite::driver("twitter")->user();
-        }
-        catch (\Exception $e) {
+        } catch (\Exception $e) {
             return redirect('/login')->with('oauth_error', 'ログインに失敗しました');
             // エラーならログイン画面へ転送
         }
         // token（メールアドレスと違ってソーシャルアカウントで不変）を持ったユーザがなければ作る
         // あれば token を持ったユーザを返す
-        $myinfo = User::firstOrCreate(['oauth_token' => $user->token ],
-            ['name' => $user->nickname,'email' => $user->getEmail(), 'oauth_token' => $user->token]);
-        Auth::login($myinfo);
-        return redirect()->to('/'); // homeへ転送
-
+        $myinfo = User::firstOrCreate(['oauth_token' => $user->token],
+            ['name' => $user->nickname, 'email' => $user->getEmail(), 'oauth_token' => $user->token]);
+        $token = auth()->login($myinfo);
+        return response()->json(['access_token' => $token, "token_type" => "bearer",
+            "expires_in" => 3600]); // homeへ転送
     }
 }
