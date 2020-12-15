8d445231e150c0dcb5c77dbbe672679a08447ad6
TetsuFe <tetsubeg1ner27@gmail.com>
2020-12-16 05:37:52 +0900

    Rewrite gacha test for new logic

--- tests/Feature/GachaTest.php
+++ tests/Feature/GachaTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\User;
+use App\Character;
 
 class GachaTest extends TestCase
 {
@@ -28,9 +29,14 @@ class GachaTest extends TestCase
         $user = factory(User::class)->create();
         $this->be($user);
 
+        $character1 = new Character(['id'=>1, 'name'=>'赤ホクマ']);
+        $character2 = new Character(['id'=>2, 'name'=>'青ホクマ']);
+        $character1->save();
+        $character2->save();
+
         $response = $this->get('api/gacha/platinum');
         $response->assertStatus(200);
-        $expectedGachaResultRange = range(1,5);
+        $expectedGachaResultRange = range(1,2);
         $this->assertTrue(in_array($response['characterId'], $expectedGachaResultRange, true));
     }
 }
